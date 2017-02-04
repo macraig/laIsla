adb57ae57d6ca6f9ce16e70ddb25d89fbdb37989
nonopautasso <eleonora.pautasso@oxed.com.ar>
2017-02-04 20:29:34 -0300

    fusionde escenas

---
+++ Assets/DistancesMapGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vectrosity;
+
+public class DistancesMapGenerator : MonoBehaviour
+{
+
+    public List<GameObject> Places; 
+    public Texture dottedLineMaterial; 
+    public Material DottedLineMateriallMaterial; 
+
+	// Use this for initialization
+	void Start () {
+	    foreach (GameObject place in Places)
+	    {	        
+            foreach (GameObject otherPlace in Places)
+	        {
+                Vector3 position = place.transform.position;
+
+                if (place.Equals(otherPlace)) continue;
+	            if (Random.Range(0f, 1f) < 0.1)
+	            {
+                    Vector3 otherPosition = otherPlace.transform.position;	           
+                    VectorLine line = new VectorLine("Curve", new List<Vector2> { new Vector2(position.x, position.y), new Vector2(otherPosition.x, otherPosition.y) }, null, 8.0f, LineType.Continuous);
+
+	                line.material = DottedLineMateriallMaterial;
+                    line.textureScale = 1f;
+                    
+                    line.Draw();
+                    Debug.Log(line.GetLength());
+                }
+	        }
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
